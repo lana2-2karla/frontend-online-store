3b4bfa1047ac690dca63301513cbf656a9d1a5f7
unknown <gmgustavom@gmail.com>
2022-02-09 18:46:45 -0300

    test: props validation for Product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Product extends Component {
   render() {
@@ -17,3 +18,9 @@ export default class Product extends Component {
     );
   }
 }
+
+Product.propTypes = {
+  title: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+};
