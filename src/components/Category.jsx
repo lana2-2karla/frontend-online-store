b9b2d6bccd13deb6c68b3a74c98a60f9590e54b4
Allana Karla <allanakarla1702@gmail.com>
2022-02-10 17:29:07 -0300

    creat function handleClick

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,6 +4,7 @@ import * as api from '../services/api';
 class Category extends Component {
     state = {
       allCategory: [],
+
     };
 
   componentDidMount = async () => {
@@ -13,22 +14,40 @@ class Category extends Component {
     });
   }
 
-  render() {
-    const { allCategory } = this.state;
-    return (
-      <div>
-        <ul>
-          { allCategory.map((elem) => (
-            <li key={ elem.id } data-testid="category">
-              { elem.name }
-              <input type="radio" />
-            </li>
-          ))}
-        </ul>
-      </div>
+handleClick = async ({ target }) => {
+  const { allCategory } = this.state;
+  // allCategory.find((element) => {
+  //   target.value
+  // })
+  console.log(target.value);
+  console.log(allCategory);
+  // target.value === on && this.setState({})
+  // this.setState({ value: true });
+  // const listProducts = await getCategoriesId(categoryId);
+  // console.log(listProducts);
+};
 
-    );
-  }
+render() {
+  const { allCategory } = this.state;
+  return (
+    <div>
+      <ul>
+        { allCategory.map((elem) => (
+          <li key={ elem.id } data-testid="category">
+            { elem.name }
+            <input
+              value={ elem.id }
+              name="category"
+              onChange={ this.handleClick }
+              type="radio"
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+
+  );
+}
 }
 
 export default Category;
--- src/services/api.js
+++ src/services/api.js
@@ -11,3 +11,9 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const data = await response.json();
   return data;
 }
+export async function getCategoriesId(categoryId) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
