97629de049daf7fcba17fa64098c20fd7d287133
Thiaracosta <thiaramscosta@gmail.com>
2022-02-10 15:48:49 -0300

    merge: correçao do Eslint

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,39 +25,38 @@ class Home extends Component {
     const { queryInput, searchedProducts, searched } = this.state;
 
     return (
-      <>
+      <main>
         <Header />
-        <main>
-          <form>
-            <label htmlFor="query-input" data-testid="home-initial-message">
-              Digite algum termo de pesquisa ou escolha uma categoria.
-              <div className="search-input">
-                <input
-                  id="query-input"
-                  value={ queryInput }
-                  onChange={ this.handleChange }
-                  data-testid="query-input"
-                  type="text"
-                />
-                <button
-                  type="submit"
-                  onClick={ this.searchButton }
-                  data-testid="query-button"
-                >
-                  Pesquisar
-                </button>
-              </div>
-            </label>
-
-            <div className="product-area">
-              { searchedProducts.length > 0
-                ? searchedProducts
-                  .map((product) => <Product key={ product.id } { ...product } />)
-                : searched && <span>Nenhum produto foi encontrado</span> }
+        <form>
+          <label htmlFor="query-input" data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+            <div className="search-input">
+              <input
+                id="query-input"
+                value={ queryInput }
+                onChange={ this.handleChange }
+                data-testid="query-input"
+                type="text"
+              />
+              <button
+                type="submit"
+                onClick={ this.searchButton }
+                data-testid="query-button"
+              >
+                Pesquisar
+              </button>
             </div>
-          </form>
-        </main>
-      </>
+          </label>
+
+          <div className="product-area">
+            { searchedProducts.length > 0
+              ? searchedProducts
+                .map((product) => <Product key={ product.id } { ...product } />)
+              : searched && <span>Nenhum produto foi encontrado</span> }
+          </div>
+        </form>
+      </main>
+
     );
   }
 }
