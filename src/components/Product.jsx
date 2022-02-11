1251e2c4838aa2d408d1e19e4d6722484a465769
unknown <gmgustavom@gmail.com>
2022-02-10 23:57:19 -0300

    sending to Product page when click on product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default class Product extends Component {
   render() {
-    const { title, price, thumbnail } = this.props;
+    const { title, price, thumbnail, id } = this.props;
     return (
       <div className="product" data-testid="product">
-        <div className="product-area-image">
-          <img className="product-image" src={ thumbnail } alt={ title } />
-        </div>
+        <Link to={ `product/${id}` }>
+          <div className="product-area-image">
+            <img className="product-image" src={ thumbnail } alt={ title } />
+          </div>
 
-        <div className="product-details">
-          <span className="product-price">{ `R$ ${price}` }</span>
-          <span className="product-title">{ title }</span>
-        </div>
+          <div className="product-details">
+            <span className="product-price">{ `R$ ${price}` }</span>
+            <span className="product-title">{ title }</span>
+          </div>
+        </Link>
       </div>
     );
   }
