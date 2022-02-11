42dac09233f9c26003b2dd734c11343cd996be5d
unknown <gmgustavom@gmail.com>
2022-02-11 15:36:25 -0300

    rendering picture and price on product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,19 +5,30 @@ import * as api from '../services/api';
 export default class Product extends Component {
   state = {
     productDetails: {},
+    loading: true,
   }
 
   async componentDidMount() {
     const { match: { params: { id } } } = this.props;
-    this.setState({ productDetails: await api.getProductDetails(id) });
+    this.setState({ productDetails: await api.getProductDetails(id), loading: false });
   }
 
   render() {
-    const { productDetails: { title } } = this.state;
+    const { loading, productDetails: { title, price, pictures } } = this.state;
+
     return (
-      <div>
-        <h2 data-testid="product-detail-name">{ title }</h2>
-      </div>
+      loading ? <p>Carregando...</p>
+        : (
+          <div className="product-details-area">
+            <div className="">
+              <img className="" src={ pictures[0].url } alt={ title } />
+            </div>
+            <div className="product-details">
+              <h2 data-testid="product-detail-name">{ title }</h2>
+              <h3 className="product-price">{ `R$ ${price.toFixed(2)}` }</h3>
+            </div>
+          </div>
+        )
     );
   }
 }
