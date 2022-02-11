8a27b92d79298b11ac9b12a1148547242859caed
unknown <gmgustavom@gmail.com>
2022-02-10 23:30:15 -0300

    feat: creating Product component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import ShoppingCart from '../pages/ShoppingCart';
+import Product from '../pages/Product';
 
 export default class Content extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class Content extends Component {
       <Switch>
         <Route exact path="/" component={ Home } />
         <Route path="/ShoppingCart" component={ ShoppingCart } />
+        <Route path="/Product/:id" component={ Product } />
       </Switch>
     );
   }
