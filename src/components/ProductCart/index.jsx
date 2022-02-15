42f8fb11811278b36844e17a01ad084644531995
guthias <gmgustavom@gmail.com>
2022-02-15 00:25:41 -0300

    style: styling ProductCart component

--- src/components/ProductCart/index.jsx
+++ src/components/ProductCart/index.jsx
@@ -1,42 +1,60 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { FaTruck } from 'react-icons/fa';
+import './style.css';
 
 export default class ProductCart extends Component {
   render() {
     const { thumbnail, title, price, quantity, increaseCart, decreaseCart,
       shipping: { free_shipping: freeShipping } } = this.props;
     return (
-      <div>
-        <div>
-          <img src={ thumbnail } alt={ title } />
+      <div className="cart-product">
+        <div className="cart-product-image-area">
+          <img className="cart-product-image" src={ thumbnail } alt={ title } />
         </div>
-        <div data-testid="shopping-cart-product-name">
-          { title }
-        </div>
-        <div>
-          { `R$ ${price.toFixed(2)}` }
-        </div>
-        <div>
-          {
-            freeShipping && <span data-testid="free-shipping">Frete Grátis</span>
-          }
-        </div>
-        <div>
-          <button
-            type="button"
-            data-testid="product-decrease-quantity"
-            onClick={ decreaseCart }
-          >
-            -
-          </button>
-          <span data-testid="shopping-cart-product-quantity">{ quantity }</span>
-          <button
-            type="button"
-            data-testid="product-increase-quantity"
-            onClick={ increaseCart }
-          >
-            +
-          </button>
+        <div className="cart-product-details">
+          <div className="cart-product-details-row">
+            <span data-testid="shopping-cart-product-name">
+              { title }
+            </span>
+            <span className="cart-product-price">
+              { `R$ ${price.toFixed(2)}` }
+            </span>
+          </div>
+          <div className="cart-product-details-row">
+            {
+              freeShipping && (
+                <span data-testid="free-shipping" className="free-shipping">
+                  <FaTruck className="free-shipping-icon" />
+                  Frete Grátis
+                </span>)
+            }
+
+            <div className="product-quantity-area">
+              <button
+                type="button"
+                className="product-quantity-button"
+                data-testid="product-decrease-quantity"
+                onClick={ decreaseCart }
+              >
+                -
+              </button>
+              <span
+                data-testid="shopping-cart-product-quantity"
+                className="product-quantity-number"
+              >
+                { quantity }
+              </span>
+              <button
+                type="button"
+                className="product-quantity-button"
+                data-testid="product-increase-quantity"
+                onClick={ increaseCart }
+              >
+                +
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     );
