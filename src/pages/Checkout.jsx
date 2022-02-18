82d4e717fef8dcdea502644c82d4ec0e3b54abbf
Thiaracosta <thiaramscosta@gmail.com>
2022-02-18 01:04:31 -0300

    adiçao da sessao das informaçoes pessoais

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -18,22 +18,56 @@ export default class Checkout extends Component {
     )).toFixed(2);
 
     return (
-      <section>
-        <h3>Revise seu pedido</h3>
-        {productCart.map((product) => (
-          <div key={ product.id }>
-            <h4>{product.title}</h4>
-            <div>
-              <img src={ product.thumbnail } alt={ product.title } />
-            </div>
-            <p>{product.price}</p>
-          </div>))}
-        <div>
-          <h4>Valor Total</h4>
-          <p>{totalPrice}</p>
-        </div>
-      </section>
-
+      <>
+        <section>
+          <h3>Revise seu pedido</h3>
+          {productCart.map((product) => (
+            <div key={ product.id }>
+              <h4>{product.title}</h4>
+              <div>
+                <img src={ product.thumbnail } alt={ product.title } />
+              </div>
+              <p>{product.price}</p>
+            </div>))}
+          <div>
+            <h4>Valor Total</h4>
+            <p>{totalPrice}</p>
+          </div>
+        </section>
+        <section>
+          <h3>Informações do comprador</h3>
+          <input
+            type="text"
+            placeholder="Nome completo"
+            data-testid="checkout-fullname"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            data-testid="checkout-email"
+          />
+          <input
+            type="number"
+            placeholder="CPF"
+            data-testid="checkout-cpf"
+          />
+          <input
+            type="number"
+            placeholder="Telefone"
+            data-testid="checkout-phone"
+          />
+          <input
+            type="number"
+            placeholder="CEP"
+            data-testid="checkout-cep"
+          />
+          <textarea
+            placeholder="Endereço"
+            data-testid="checkout-address"
+          />
+        </section>
+        <button type="button">Finalizar compra</button>
+      </>
     );
   }
 }
