07196f0a40ea2d7135bdb0593e8fecd841a06310
guthias <gmgustavom@gmail.com>
2022-02-17 21:33:25 -0300

    saving cart on localStorage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Content from './components/Content';
 import Header from './components/Header';
+import * as api from './services/api';
 import './App.css';
 
 class App extends React.Component {
   state = {
     cart: [],
+  }
+
+  componentDidMount() {
+    this.setState({ cart: api.getLocalStorage('cart') || [] });
+  }
+
+  updateCart = (newCart) => {
+    this.setState({ cart: newCart });
+    api.saveLocalStorage('cart', newCart);
   }
 
   newCartItem = (productId, productTitle) => ({
@@ -25,7 +35,7 @@ class App extends React.Component {
       productOnCart = this.newCartItem(productId, productTitle);
       newCart.push(productOnCart);
     }
-    this.setState({ cart: newCart });
+    this.updateCart(newCart);
   };
 
   decreaseCart = (productId) => {
@@ -34,7 +44,7 @@ class App extends React.Component {
     const productOnCart = newCart.find(({ id }) => productId === id);
     if (productOnCart.quantity - 1 >= 1) {
       productOnCart.quantity -= 1;
-      this.setState({ cart: newCart });
+      this.updateCart(newCart);
     }
   }
 
@@ -44,7 +54,7 @@ class App extends React.Component {
     const productOnCart = newCart.find(({ id }) => productId === id);
     if (productOnCart.quantity + 1 <= maxQuantity) {
       productOnCart.quantity += 1;
-      this.setState({ cart: newCart });
+      this.updateCart(newCart);
     }
   }
 
